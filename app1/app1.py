62347f6dca4051762ad6895a0595e6429501b02c
Диана Залилова <dizalilova@edu.hse.ru>
2025-11-21 13:58:49 +0300

    add hw

--- app1/app1.py
+++ app1/app1.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='/storage')
 
 
 @app.route('/hello')
@@ -9,4 +9,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-   app.run(debug=True, host='0.0.0.0', port=8080)
+   app.run(debug=True, host='0.0.0.0', port=81)
--- app2/app2.py
+++ app2/app2.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='/storage')
 
 
 @app.route('/hello')
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', source_app='app2')
 
 
 if __name__ == '__main__':
-   app.run(debug=True, host='0.0.0.0', port=80)
+   app.run(debug=True, host='0.0.0.0', port=81)
